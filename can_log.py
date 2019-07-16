d12c25fa324ae506e1fb98d622c3bbcba4c74c6b
MateSzilak <szilak.mate@gmail.com>
2019-07-16 21:57:11 +0200

    Update can_log.py

--- can_log.py
+++ can_log.py
@@ -5,8 +5,8 @@ import random
 #generating a random number, creating a folder
 random_number = random.randint(100,999)
 
-os.makedirs("/home/can_log/%d" % random_number )
-file = open("/home/can_log/%d/log.txt" % random_number, "w") 
+os.makedirs("/home/%d" % random_number )
+file = open("/home/%d/log.txt" % random_number, "w") 
 
 can_interface = 'can0'
 bus = can.interface.Bus(can_interface,bustype='socketcan')
